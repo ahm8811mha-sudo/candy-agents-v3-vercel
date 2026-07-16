b289c8141ff6083fd50bd5f5bbba1e6cb00d4766
ahm8811mha-sudo <ahm8811mha@gmail.com>
2026-07-17 02:37:42 +0300

    use employee runtime v2 console

--- app/employee-runtime/page.tsx
+++ app/employee-runtime/page.tsx
@@ -1,7 +1,7 @@
-import EmployeeRuntimePanel from "@/components/EmployeeRuntimePanel";
+import EmployeeRuntimePanelV2 from "@/components/EmployeeRuntimePanelV2";
 
 export const dynamic = "force-dynamic";
 
 export default function EmployeeRuntimePage() {
-  return <EmployeeRuntimePanel />;
+  return <EmployeeRuntimePanelV2 />;
 }
