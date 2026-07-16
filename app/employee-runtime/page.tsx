69de246423dc318cbfc750eaf4db00ab3ad73815
ahm8811mha-sudo <ahm8811mha@gmail.com>
2026-07-17 02:18:33 +0300

    add employee runtime page

---
+++ app/employee-runtime/page.tsx
@@ -0,0 +1,7 @@
+import EmployeeRuntimePanel from "@/components/EmployeeRuntimePanel";
+
+export const dynamic = "force-dynamic";
+
+export default function EmployeeRuntimePage() {
+  return <EmployeeRuntimePanel />;
+}
